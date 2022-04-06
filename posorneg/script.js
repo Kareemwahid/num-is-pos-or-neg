55725f9b28017a75b611ffd7430d57d8fbb348ff
Kareemwahid <102654766+Kareemwahid@users.noreply.github.com>
2022-04-06 11:52:58 +0530

    Add files via upload

---
+++ posorneg/script.js
@@ -0,0 +1,10 @@
+var Num=parseInt(prompt('Enter a Number'))
+
+if(Num>0)
+console.log('IS a positive number');
+
+else if (Num==0)
+console.log('IS a Neutral');
+
+else if (Num<0)
+console.log("IS a Negative number");
